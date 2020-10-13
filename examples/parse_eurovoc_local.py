6a9d2c396903fed20e0b584a0a69e4aa20e861f6
neuro <laurensmeeus93@gmail.com>
2020-10-13 15:27:49 +0200

    download eurovoc and local parsing.

---
+++ examples/parse_eurovoc_local.py
@@ -0,0 +1,28 @@
+import rdflib
+
+from media.data import get_eurovoc_rdf
+
+
+def main():
+    """
+    Way too slow
+    Returns:
+
+    """
+    raise DeprecationWarning()
+
+    g = rdflib.Graph()
+
+    filename_rdf = get_eurovoc_rdf()
+    g.parse(filename_rdf)
+
+    print(len(g))
+
+    for s, p, o in g:
+        print(s, p, o)
+
+    return g
+
+
+if __name__ == '__main__':
+    main()
---
+++ media/data.py
@@ -0,0 +1,57 @@
+import requests
+import os.path
+from zipfile import ZipFile
+
+import requests
+
+
+def get_eurovoc_rdf():
+    """ Has 3,541,287 triplets.
+
+    :return:
+    """
+
+    media_folder = os.path.dirname(__file__)
+
+    filename_rdf = 'eurovoc_skos.rdf'
+
+    filename_zip = os.path.join(media_folder, 'eurovoc_skos.zip')
+    url = 'http://publications.europa.eu/resource/distribution/eurovoc/20200630-0/zip/skos_xl/eurovoc_skos.zip'
+
+    download_if_not_exists(filename_zip, url)
+
+    filepath_rdf = os.path.join(media_folder, filename_rdf)
+    if not os.path.exists(filepath_rdf):
+        download_if_not_exists(filename_zip, url)
+
+        with ZipFile(filename_zip, 'r') as zipObj:
+            zipObj.extract(filename_rdf, path=media_folder)
+
+    return filepath_rdf
+
+
+def download_file(filename, url):
+    """
+    Download an URL to a file
+    """
+    with open(filename, 'wb') as fout:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        # Write response data to file
+        for block in response.iter_content(2**12):
+            fout.write(block)
+
+
+def download_if_not_exists(filename, url):
+    """
+    Download a URL to a file if the file
+    does not exist already.
+    Returns
+    -------
+    True if the file was downloaded,
+    False if it already existed
+    """
+    if not os.path.exists(filename):
+        download_file(filename, url)
+        return True
+    return False
