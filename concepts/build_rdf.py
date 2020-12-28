808626f28bfe7ce1e3a14ee63a3f5310a880ae7d
neuro <laurensmeeus93@gmail.com>
2020-12-28 17:03:39 +0100

    Similar terms RDF Graph

--- concepts/build_rdf.py
+++ concepts/build_rdf.py
@@ -1,10 +1,12 @@
-from rdflib import Literal, Namespace, Graph
+from typing import Dict, List
+
+from rdflib import Literal, Namespace, Graph, URIRef
 from rdflib.namespace import SKOS, RDF
 
 from reporting_obligations.build_rdf import NS_BASE
 
 CONCEPT_BASE = Namespace(NS_BASE + 'concepts/')
-LANG = 'en'
+EN = 'en'
 
 
 class ConceptGraph(Graph):
@@ -27,22 +29,58 @@ class ConceptGraph(Graph):
 
         self.uid_iterator = UIDIterator()
 
-    def add_terms(self, l_terms, lang=LANG):
-        """ Build the RDF from cas content.
+    def add_terms(self, l_terms: List[str], lang=EN):
+        """ Add new terms to the RDF as SKOS concepts.
+
+        Args:
+            l_terms: List of the terms in string format
+            lang: optional language parameter of the terms.
+
+        Returns:
+            list of RDF URI's of the new SKOS concepts.
         """
 
+        l_terms = list(map(str, l_terms)) #
+        l_uri = [None for _ in l_terms]    # Initialisation
         for i, term_i in enumerate(l_terms):
             node_term_i = self.uid_iterator.get_next()
 
+            l_uri[i] = node_term_i
+
             self.add((node_term_i,
                       RDF.type,
-                      RDF.Property
+                      SKOS.Concept
                       ))
 
             self.add((node_term_i,
                       SKOS.prefLabel,
                       Literal(term_i, lang=lang)
                       ))
+
+        return l_uri
+
+
+class LinkConceptGraph(ConceptGraph):
+    """
+    Builds further on a concept Graph but can be seen solely to add relationships between different glossaries
+    """
+
+    def add_similar_terms(self, sim_terms=Dict[str, List[str]]):
+        """
+
+        Args:
+            sim_terms: Dictionary for similar term matches with the URI's of the
+
+        Returns:
+            None
+        """
+
+        for uri_i, l_matches_uri in sim_terms.items():
+            for uri_j in l_matches_uri:
+                self.add((URIRef(uri_i),
+                          SKOS.relatedMatch,
+                          URIRef(uri_j)
+                          ))
 
 
 class UIDIterator:
