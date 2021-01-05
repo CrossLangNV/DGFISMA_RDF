065754c9373867da8987eb5ad3e114fbe9aef216
neuro <laurensmeeus93@gmail.com>
2021-01-05 13:20:52 +0100

    Test setup for pip install through git.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('docker/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='RDF for DGFISMA',
+    version='0.1',
+    description='Module for exporting reporting obligations and glossary to RDF.',
+    # license="MIT",
+    long_description=long_description,
+    author='Laurens Meeus',
+    author_email='laurens.meeus@crosslang.com',
+    url="http://www.crosslang.com/",
+    packages=['DGFISMA_RDF'],  # same as name
+    install_requires=required,  # external packages as dependencies
+    # scripts=[
+    #          'scripts/cool',
+    #          'scripts/skype',
+    #         ]
+)
